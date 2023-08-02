dad4d2ea01c1b3e69f7c200c6301be8ad34ffdef
Carcamo-ben <carcamo.ben@gmail.com>
2023-08-02 15:00:21 -0400

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Que dice el documento XXXXX",
+        value: "Que dice el documento XXXXX"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Quien es el asesino del Sr Hendricks?", value: "Quien es el asesino del Sr Hendricks?" },
+    { text: "Que hace la FACH", value: "Que hace la FACH" }
 ];
 
 interface Props {
